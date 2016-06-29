5e451abd2271b119eeaf4296b00d8bb8e73bc7c5
Sergio Tapia <sergiotapiagutierrez@gmail.com>
2016-06-29 10:34:05 -0400

    Add in logging, and some better tests and return structures.

--- lib/github_users.ex
+++ lib/github_users.ex
@@ -1,7 +1,9 @@
 defmodule GithubUsers do
+  require Logger
+  
   def search(url) do
     cond do
-      url == "" -> { :error }
+      url == "" -> { :error, "Can't search for an empty string" }
       String.contains?(url, "/orgs/") -> scrape_organization(url)
       String.contains?(url, "/search?") -> scrape_search(url)
       String.contains?(url, "/stargazers") -> {:ok, %{github_id: "sergiotapiaz"}}
@@ -12,15 +14,19 @@ defmodule GithubUsers do
   defp scrape_profile(url) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, %{fullname: Floki.find(body, "div.vcard-fullname") |> Floki.text,
-          username: Floki.find(body, "div.vcard-username") |> Floki.text,
-          location: Floki.find(body, "li[itemprop=homeLocation]") |> Floki.text,
-          email: Floki.find(body, "[itemprop=email]") |> Floki.text,
-          website: Floki.find(body, "[itemprop=url]") |> Floki.text,
-          join_date: Floki.find(body, ".join-date") |> Floki.text,
-          followers: Floki.find(body, ".vcard-stat-count") |> Enum.at(0) |> Floki.text,
-          starred: Floki.find(body, ".vcard-stat-count") |> Enum.at(1) |> Floki.text,
-          following: Floki.find(body, ".vcard-stat-count") |> Enum.at(2) |> Floki.text}}
+        join_date = Floki.find(body, ".octicon-clock")
+        if Enum.count(join_date) == 1 do
+          Logger.info("Scraping profile: #{url}")
+          %{fullname: Floki.find(body, "div.vcard-fullname") |> Floki.text,
+            username: Floki.find(body, "div.vcard-username") |> Floki.text,
+            location: Floki.find(body, "li[itemprop=homeLocation]") |> Floki.text,
+            email: Floki.find(body, "[itemprop=email]") |> Floki.text,
+            website: Floki.find(body, "[itemprop=url]") |> Floki.text,
+            join_date: Floki.find(body, ".join-date") |> Floki.text,
+            followers: Floki.find(body, ".vcard-stat-count") |> Enum.at(0) |> Floki.text,
+            starred: Floki.find(body, ".vcard-stat-count") |> Enum.at(1) |> Floki.text,
+            following: Floki.find(body, ".vcard-stat-count") |> Enum.at(2) |> Floki.text}
+        end
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         {:error, "User profile not found"}
       {:error, %HTTPoison.Error{reason: reason}} ->
@@ -31,7 +37,9 @@ defmodule GithubUsers do
   defp scrape_organization(url) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Floki.find(body, "a.member-link") |> Floki.attribute("href") |> Enum.map(fn(x) -> "https://github.com#{x}" end) |> Enum.map(fn(x) -> scrape_profile(x) end ) }
+        Floki.find(body, "a.member-link") |> Floki.attribute("href")
+                                          |> Enum.map(fn(x) -> "https://github.com#{x}" end)
+                                          |> Enum.map(fn(x) -> scrape_profile(x) end )
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         {:error, "User profile not found"}
       {:error, %HTTPoison.Error{reason: reason}} ->
@@ -42,12 +50,13 @@ defmodule GithubUsers do
   defp scrape_search(url) do
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Floki.find(body, "div.user-list-item a") |> Floki.attribute("href")
-                                                       |> Enum.uniq
-                                                       |> Enum.reject(fn(x) -> String.contains?(x, "/login") end)
-                                                       |> Enum.reject(fn(x) -> String.contains?(x, "mailto:") end)
-                                                       |> Enum.map(fn(x) -> "https://github.com#{x}" end)
-                                                       |> Enum.map(fn(x) -> scrape_profile(x) end ) }
+        Floki.find(body, "div.user-list-item a") |> Floki.attribute("href")
+                                                 |> Enum.uniq
+                                                 |> Enum.reject(fn(x) -> String.contains?(x, "/login") end)
+                                                 |> Enum.reject(fn(x) -> String.contains?(x, "mailto:") end)
+                                                 |> Enum.map(fn(x) -> "https://github.com#{x}" end)
+                                                 |> Enum.map(fn(x) -> scrape_profile(x) end )
+                                                 |> Enum.reject(fn(x) -> x == nil end)
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         {:error, "User profile not found"}
       {:error, %HTTPoison.Error{reason: reason}} ->
--- test/github_users_test.exs
+++ test/github_users_test.exs
@@ -2,31 +2,31 @@ defmodule GithubUsersTest do
   use ExUnit.Case
   doctest GithubUsers
 
-  # test "scrapes user profile" do
-  #   { :ok, user } = GithubUsers.search("https://github.com/sergiotapia")
-  #   assert user[:email] == "sergiotapia@outlook.com"
-  #   assert is_integer(user[:followers]) == true
-  #   assert is_integer(user[:following]) == true
-  #   assert is_integer(user[:starred]) == true
-  #   assert user[:fullname] == "Sergio Tapia"
-  #   assert user[:join_date] == "Mar 23, 2011"
-  #   assert user[:location] == "Miami"
-  #   assert user[:username] == "sergiotapia"
-  #   assert user[:website] == "http://sergiotapia.me"
-  # end
-
-  # test "scrapes an organization" do
-  #   { :ok, users } = GithubUsers.search("https://github.com/orgs/elixir-lang/people")
-  #   assert true == true
-  # end
+  test "scrapes user profile" do
+    user = GithubUsers.search("https://github.com/sergiotapia")
+    assert user[:email] == "sergiotapia@outlook.com"
+    assert user[:followers] != nil
+    assert user[:following] != nil
+    assert user[:starred] != nil
+    assert user[:fullname] == "Sergio Tapia"
+    assert user[:join_date] == "Mar 23, 2011"
+    assert user[:location] == "Miami"
+    assert user[:username] == "sergiotapia"
+    assert user[:website] == "http://sergiotapia.me"
+  end
 
   test "scrapes an organization" do
-    { :ok, users } = GithubUsers.search("https://github.com/search?q=elixir&type=Users&utf8=%E2%9C%93")
-    assert true == true
+    users = GithubUsers.search("https://github.com/orgs/elixir-lang/people")
+    assert is_list(users) == true
+  end
+
+  test "scrapes a search" do
+    users = GithubUsers.search("https://github.com/search?q=elixir&type=Users&utf8=%E2%9C%93")
+    assert is_list(users) == true
   end
 
   test "return error on empty string" do
-    { result } = GithubUsers.search("")
-    assert result == :error
+    { :error, reason} = GithubUsers.search("")
+    assert reason == "Can't search for an empty string"
   end
 end
